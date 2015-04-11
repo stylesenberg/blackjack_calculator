902422406738192cfe557d5e8fc2218312b058c5
walter stylesenberg <stylesenberg@walters-MacBook-Air.local>
2015-04-11 16:49:53 +0200

    tests global variables updated

--- js/tests/blackjack.tests.js
+++ js/tests/blackjack.tests.js
@@ -1,9 +1,41 @@
-QUnit.test( "css & js", function( assert ) {
-	var link1 = $("head link:eq(0)").attr("href");
-	var link2 = $("head link:eq(1)").attr("href");
-	var link3 = $("head link:eq(2)").attr("href");
+QUnit.test( "global variables", function( assert ){
+  var test1 = typeof game;
+  var test2 = function() {
+    var length = 0;
+    for (var property in game) {
+      if (game.hasOwnProperty(property)){
+        length += 1;
+      }
+    };
+    return length;
+  };
+  var test3 = game.bankroll;
+  var test4 = game.betSize;
+  var test5 = game.deck.length;
+  var test6 = game.playersHand.length;
+  var test7 = game.dealersHand.length;
+  var test8 = game.valueOfPlayersHand;
+  var test9 = game.valueOfDealersHand;
 
-  assert.equal( link1, "css/bootstrap.min.css", "bootstrap.min.css is included." );
-  assert.equal( link2, "css/qunit.css", "qunit.css is included." );
-  assert.equal( link3, "css/custom.css", "custom.css is included." );
+
+
+  assert.equal( test1, "object", "Typeof global variable 'game' equals 'object'." );
+  assert.equal( test2(), 7, "Global variable 'game' has 7 properties." );
+  assert.equal( test3, 100, "Property game.bankroll is set to 100." );
+  assert.equal( test4, 10, "Property game.betSize is set to 10." );
+  assert.equal( test5, 0, "Property game.deck is an empty array.");
+  assert.equal( test6, 0, "Property game.playersHand is an empty array.");
+  assert.equal( test7, 0, "Property game.dealersHand is an empty array.");
+  assert.equal( test8, 0, "Property game.valueOfPlayersHand is set to 0.");
+  assert.equal( test9, 0, "Property game.valueOfDealersHand is set to 0.");
+});
+
+QUnit.test( "function shuffle()", function( assert ) {
+	var shuffle_length = shuffle.length;
+	var arr1 = [0,1,2,3,4];
+	var arr2 = [0,1,2,3,4];
+	var shuffled_arr = shuffle(arr1);
+
+  assert.equal( shuffle_length, "1", "function shuffle() is defined." );
+  assert.notEqual( arr1, arr2, "function shuffle() works.");
 });
